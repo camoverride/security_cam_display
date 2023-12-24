7c5d51e2257e237b22ffb930068376555c0d4225
cam <cam@cams-MacBook-Pro.local>
2023-12-23 16:34:23 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Image Display
+
+Display recent images from a folder to the screen.
+
+
+## Setup
+
+Install the python requirements: `pip install --file requirements.txt`.
+
+
+## Run
+
+- `export FLASK_APP=front_end`
+- `flask run`
+- View in Firefox browser: `http://127.0.0.1:5000`
+- Eliminate border and search bar: 
+- TODO: this should be run as a standalone file: `file://display.html`` in the browser!
---
+++ front_end.py
@@ -0,0 +1,37 @@
+import os
+import time
+from threading import Thread
+
+from flask import Flask, render_template
+
+
+
+ARCHIVE = "/Desktop/whisper_server/archive/"
+UPDATE_INTERVAL = 5 # seconds
+MAX_FILES = 16 # Maximum number of face images to be copied. Should be greater than or equal to the # needed.
+
+
+# Set up Flask
+app = Flask(__name__)
+
+# Render the front-end
+@app.route("/")
+def index():
+    return render_template("display.html")
+
+# Update files in `static/archive`
+def get_new_files():
+    while True:
+        print("UPDATING archive contents!")
+        files = os.listdir(ARCHIVE)
+        files_new_to_old = list(reversed(sorted(files)))
+        
+        # Rename files as 1.jpg, 2.jpg etc
+        for index, filename in enumerate(files_new_to_old)[:MAX_FILES]:
+            os.system(f"mv {ARCHIVE}/{filename} static/archive/{index}.jpg")
+
+        time.sleep(UPDATE_INTERVAL)
+
+
+t = Thread(target=get_new_files)
+t.start()
