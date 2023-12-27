b996626ad69d9078bc7060aeda76dbefd9bc7628
cam <cam@cams-MacBook-Pro.local>
2023-12-27 15:27:07 -0800

    debug

--- update_archive.py
+++ update_archive.py
@@ -4,12 +4,14 @@ import logging
 
 
 
+logging.basicConfig(level=logging.DEBUG)
+
 ARCHIVE_CHECK_INTERVAL = 10 # seconds
 ARCHIVE_REMOTE_LOCATION = "cam@cam-machine.local:~/security_cam_display/static/archive"
 PASSWORD = "a"
 
 def update_archive():
-    logging.warning("Updating archive!")
+    logging.debug("Updating archive!")
     try:
         # Get the files
         os.system(f"sshpass -p {PASSWORD} scp -r {ARCHIVE_REMOTE_LOCATION} static/archive")
