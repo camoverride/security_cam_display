17cc240e2a2bbd8dffb60b980689bd99d511d3c2
cam <cam@cams-MacBook-Pro.local>
2023-12-27 00:42:27 -0800

    add local file copier

---
+++ update_archive.py
@@ -0,0 +1,20 @@
+import os
+import time
+import logging
+
+
+
+ARCHIVE_CHECK_INTERVAL = 10 # seconds
+ARCHIVE_REMOTE_LOCATION = "cam@cam-machine.local:~/security_cam_display/static/archive"
+PASSWORD = "a"
+
+
+while True:
+    try:
+        # Get the files
+        os.system(f"sshpass -p {PASSWORD} scp -r {ARCHIVE_REMOTE_LOCATION} static/archive")
+
+    except Exception as e:
+        logging.warn(e)
+    
+    time.sleep(ARCHIVE_CHECK_INTERVAL)
